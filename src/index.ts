1ad89495eef07a18bdf95394bc3162c6962c697b
ZZCWC <85448514+losier@users.noreply.github.com>
2022-12-23 22:36:54 +0530

    added uuid and made a based module

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,11 @@ import chalkAnimation from 'chalk-animation';
 import figlet from 'figlet';
 import { createSpinner } from 'nanospinner';
 import inquirer from 'inquirer';
+import { v4 as uuidv4 } from 'uuid';
 
 const sleep = (ms = 1200) => new Promise((r) => setTimeout(r, ms));
+const user_id = uuidv4();
+var startingMsg = 'Hi...\n';
 
 async function welcome() {
   const title = welcomeTxt[Math.floor(Math.random() * welcomeTxt.length)];
@@ -24,6 +27,25 @@ async function welcome() {
   });
 }
 
-async function chat_with_me() {}
+async function chat() {
+  const question = await inquirer.prompt({
+    name: 'question',
+    type: 'input',
+    message: `${startingMsg}`,
+  });
+  const res = await fetch(
+    `http://api.brainshop.ai/get?bid=171368&key=Csf9rns7uWZFQ0dj&uid=${user_id}&msg=${question}`,
+  ).then((response) => response.json());
+
+  const reply = res.cnt;
+  if (reply == '') return;
+  startingMsg = reply;
+  await chat();
+}
+
+async function chat_with_me() {
+  await welcome();
+  await chat();
+}
 
 chat_with_me();
